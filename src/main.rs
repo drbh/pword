71959be7ed99e8a2fe510bc2a232f153c833361f
drbh <david.richard.holtz@gmail.com>
2020-01-12 21:13:16 -0500

    remove unwraps

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-#[macro_use]
-extern crate clap;
-extern crate chbs;
-extern crate rpassword;
-
 use argon2::{self, Config};
 use chbs::{config::BasicConfig, prelude::*, probability::Probability};
+use clap::clap_app;
 use rpassword::read_password;
-
 use std::io;
 
 fn main() {
@@ -15,23 +10,24 @@ fn main() {
         (author: "David H. <david.richard.holtz@gmail.com>")
         (about: "🔥 a deterministic password generator 🤘")
         (@subcommand new =>
-            (about: "create new master phrase")
+            (about: "create new master pass phrase")
         )
         (@subcommand generate =>
             (about: "generate new password")
-            // (@arg INPUT: +required "Sets the new password host")
         )
     )
     .get_matches();
-
+    // flag if user does nothing
+    let mut was_called = false;
     if let Some(_matches) = matches.subcommand_matches("new") {
+        was_called = true;
         let phrase = create_new_phrase(3);
         println!("{}", phrase);
     }
-
     if let Some(_matches) = matches.subcommand_matches("generate") {
-        println!("Type a password: ");
-        let password = read_password().unwrap();
+        was_called = true;
+        println!("Type your pass phrase: ");
+        let password = read_password().expect("You did not enter a password!");
         println!("👍 thanks.\n");
         println!("Type a label for your password");
         let mut input = String::new();
@@ -41,11 +37,17 @@ fn main() {
         let my_input = input.as_bytes();
         let salt = password.as_bytes();
         let config = Config::default();
-        let hash = argon2::hash_encoded(my_input, salt, &config).unwrap();
-        let _does_matches = argon2::verify_encoded(&hash, my_input).unwrap();
-        println!("\nYour password:");
-        let ss: String = hash.chars().skip(40).take(20).collect();
-        println!("{}", ss);
+        match argon2::hash_encoded(my_input, salt, &config) {
+            Ok(hash) => {
+                println!("\nYour password:");
+                let ss: String = hash.chars().skip(40).take(20).collect();
+                println!("{}", ss)
+            }
+            Err(e) => println!("{:?} please use a longer pass phrase", e),
+        };
+    }
+    if was_called == false {
+        println!("You did not specify [new] or [generate] type --help for more infomation");
     }
 }
 
@@ -54,7 +56,6 @@ fn create_new_phrase(word_count: usize) -> String {
     config.words = word_count;
     config.separator = " ".into();
     config.capitalize_first = Probability::from(0.33);
-
     let mut scheme = config.to_scheme();
     scheme.generate()
 }
